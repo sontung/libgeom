ac02488ade60c8e2296ea626ac96065295b582d6
Son Tung <tungns@hblab.vn>
2021-05-18 10:43:08 +0700

    fast fixing mc

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 including personal geometry processing algorithms in Python
 
 # dependencies
-# open3d
-```
-
-```
+### open3d
+### meshio
+### pykdtree
--- math_utils.py
+++ math_utils.py
@@ -1,6 +1,29 @@
 import numpy as np
 
 
+def cross(a, b):
+    """
+    Calculate cross product of 2 vector a and b. a, b is array has shape (3, 1)
+    :param a:
+    :param b:
+    :return:
+    """
+    a1, a2, a3 = a
+    b1, b2, b3 = b
+    ss = [a2*b3 - a3*b2, a3*b1 - a1*b3, a1*b2 - a2*b1]
+    return np.array(ss)
+
+
+def dot(a, b):
+    """
+    Calculate dot product of 2 vector a and b. a, b is array has shape (3, 1)
+    :param a:
+    :param b:
+    :return:
+    """
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+
+
 def ray_intersects_triangle(ray_origin, ray_vector, triangle):
     """
     Möller–Trumbore ray-triangle intersection algorithm
@@ -16,20 +39,22 @@ def ray_intersects_triangle(ray_origin, ray_vector, triangle):
     vertex2 = triangle[2]
     edge1 = vertex1 - vertex0
     edge2 = vertex2 - vertex0
-    h = np.cross(ray_vector, edge2)
-    a = np.dot(edge1, h)
+    h = cross(ray_vector, edge2)
+
+    a = dot(edge1, h)
+
     if -epsilon < a < epsilon:
         return None
     f = 1.0 / a
     s = ray_origin - vertex0
-    u = f * np.dot(s, h)
+    u = f * dot(s, h)
     if u < 0.0 or u > 1.0:
         return None
-    q = np.cross(s, edge1)
-    v = f * np.dot(ray_vector, q)
+    q = cross(s, edge1)
+    v = f * dot(ray_vector, q)
     if v < 0.0 or u+v > 1.0:
         return None
-    t = f * np.dot(edge2, q)
+    t = f * dot(edge2, q)
     if t > epsilon:
         res = ray_origin + ray_vector * t
         return res
