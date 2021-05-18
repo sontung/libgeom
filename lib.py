745979244d2b4cd2b877443e575dd1bab7366c23
Son Tung <tungns@hblab.vn>
2021-05-18 10:30:15 +0700

    mc

--- lib.py
+++ lib.py
@@ -1,45 +1,127 @@
 import meshio
 import numpy as np
 import utils
-import math
+import marching_cube
+import math_utils
 import open3d as o3d
 from scipy.spatial.distance import euclidean as distance_function
+from pykdtree.kdtree import KDTree as Fast_Kd_Tree
+from collections import namedtuple
 
 
-def ray_intersects_triangle(ray_origin, ray_vector, triangle):
+def surface_reconstruct_marching_cube(point_cloud):
     """
-    Möller–Trumbore ray-triangle intersection algorithm
-    (https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm)
-    :param ray_origin:
-    :param ray_vector:
-    :param triangle:
+    reconstruct the surface of a point cloud using marching cube algorithm
+    :param point_cloud:
     :return:
     """
-    epsilon = 0.0000001
-    vertex0 = triangle[0]
-    vertex1 = triangle[1]
-    vertex2 = triangle[2]
-    edge1 = vertex1 - vertex0
-    edge2 = vertex2 - vertex0
-    h = np.cross(ray_vector, edge2)
-    a = np.dot(edge1, h)
-    if -epsilon < a < epsilon:
-        return None
-    f = 1.0 / a
-    s = ray_origin - vertex0
-    u = f * np.dot(s, h)
-    if u < 0.0 or u > 1.0:
-        return None
-    q = np.cross(s, edge1)
-    v = f * np.dot(ray_vector, q)
-    if v < 0.0 or u+v > 1.0:
-        return None
-    t = f * np.dot(edge2, q)
-    if t > epsilon:
-        res = ray_origin + ray_vector * t
-        return res
-    else:
-        return None
+    a_step = 0.1
+
+    bounding_box = point_cloud.get_axis_aligned_bounding_box()
+    min_bound = bounding_box.min_bound-a_step
+    max_bound = bounding_box.max_bound+a_step
+
+    GridCell = namedtuple('GridCell', ['xyz', 'val'])
+    a_tree = Fast_Kd_Tree(np.asarray(point_cloud.points))
+    x_steps = np.arange(min_bound[0], max_bound[0], a_step)
+    y_steps = np.arange(min_bound[1], max_bound[1], a_step)
+    z_steps = np.arange(min_bound[2], max_bound[2], a_step)
+
+    number_cube = x_steps.shape[0] * y_steps.shape[0] * z_steps.shape[0]
+    print(min_bound, max_bound, number_cube)
+
+    cube_points2, stu2cnt, stu2cnt_dict = marching_cube.all_points_in_cube(x_steps, y_steps, z_steps, a_step)
+    dist_dict, _ = a_tree.query(cube_points2, k=1)
+    triangles = []
+    a, b, c = len(z_steps)+1, len(y_steps)+1, len(x_steps)+1
+
+    for s, z in enumerate(z_steps):
+        for t, y in enumerate(y_steps):
+            for u, x in enumerate(x_steps):
+                coord = [[x + a_step, y, z], [x, y, z], [x, y + a_step, z], [x + a_step, y + a_step, z],
+                         [x + a_step, y, z + a_step], [x, y, z + a_step], [x, y + a_step, z + a_step],
+                         [x + a_step, y + a_step, z + a_step]]
+
+                indices = [s*c*b+t*c+(u+1), s*c*b+t*c+u, s*c*b+(t+1)*c+u, s*c*b+(t+1)*c+(u+1),
+                           (s+1)*c*b+t*c+(u+1), (s+1)*c*b+t*c+u, (s+1)*c*b+(t+1)*c+u, (s+1)*c*b+(t+1)*c+(u+1)]
+                distances = [dist_dict[stu2cnt_dict[indices[0]]], dist_dict[stu2cnt_dict[indices[1]]],
+                             dist_dict[stu2cnt_dict[indices[2]]], dist_dict[stu2cnt_dict[indices[3]]],
+                             dist_dict[stu2cnt_dict[indices[4]]], dist_dict[stu2cnt_dict[indices[5]]],
+                             dist_dict[stu2cnt_dict[indices[6]]], dist_dict[stu2cnt_dict[indices[7]]]]
+                cell = GridCell(coord, distances)
+                tri, _ = marching_cube.march_cube(cell)
+                triangles.extend(tri)
+    print("created: ", len(triangles))
+    utils.create_obj_file(triangles)
+
+
+def surface_reconstruct_marching_cube_with_vis(point_cloud):
+    """
+    reconstruct the surface of a point cloud using marching cube algorithm with visualization
+    :param point_cloud:
+    :return:
+    """
+    a_step = 0.1
+
+    bounding_box = point_cloud.get_axis_aligned_bounding_box()
+    min_bound = bounding_box.min_bound-a_step
+    max_bound = bounding_box.max_bound+a_step
+
+    GridCell = namedtuple('GridCell', ['xyz', 'val'])
+    a_tree = Fast_Kd_Tree(np.asarray(point_cloud.points))
+    x_steps = np.arange(min_bound[0], max_bound[0], a_step)
+    y_steps = np.arange(min_bound[1], max_bound[1], a_step)
+    z_steps = np.arange(min_bound[2], max_bound[2], a_step)
+
+    number_cube = x_steps.shape[0] * y_steps.shape[0] * z_steps.shape[0]
+    print(min_bound, max_bound, number_cube)
+
+    cube_points2, stu2cnt, stu2cnt_dict = marching_cube.all_points_in_cube(x_steps, y_steps, z_steps, a_step)
+    dist_dict, _ = a_tree.query(cube_points2, k=1)
+    triangles = []
+    a, b, c = len(z_steps)+1, len(y_steps)+1, len(x_steps)+1
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(point_cloud)
+    ctr = vis.get_view_control()
+    current_angle = 0
+
+    for s, z in enumerate(z_steps):
+        for t, y in enumerate(y_steps):
+            for u, x in enumerate(x_steps):
+                coord = [[x + a_step, y, z], [x, y, z], [x, y + a_step, z], [x + a_step, y + a_step, z],
+                         [x + a_step, y, z + a_step], [x, y, z + a_step], [x, y + a_step, z + a_step],
+                         [x + a_step, y + a_step, z + a_step]]
+
+                indices = [s*c*b+t*c+(u+1), s*c*b+t*c+u, s*c*b+(t+1)*c+u, s*c*b+(t+1)*c+(u+1),
+                           (s+1)*c*b+t*c+(u+1), (s+1)*c*b+t*c+u, (s+1)*c*b+(t+1)*c+u, (s+1)*c*b+(t+1)*c+(u+1)]
+                distances = [dist_dict[stu2cnt_dict[indices[0]]], dist_dict[stu2cnt_dict[indices[1]]],
+                             dist_dict[stu2cnt_dict[indices[2]]], dist_dict[stu2cnt_dict[indices[3]]],
+                             dist_dict[stu2cnt_dict[indices[4]]], dist_dict[stu2cnt_dict[indices[5]]],
+                             dist_dict[stu2cnt_dict[indices[6]]], dist_dict[stu2cnt_dict[indices[7]]]]
+                cell = GridCell(coord, distances)
+                tri, cube_index = marching_cube.march_cube(cell)
+
+                if len(tri) > 0:
+                    triangles.extend(tri)
+                    dum = []
+                    for _tri in tri:
+                        dum.extend(_tri)
+                        for geom in utils.visualize_unit_cube(cell, tri):
+                            vis.add_geometry(geom)
+                            ctr.rotate(current_angle, 0.0)
+                else:
+                    for geom in utils.visualize_unit_cube(cell, None):
+                        vis.add_geometry(geom)
+                        ctr.rotate(current_angle, 0.0)
+                ctr.rotate(5.0, 0.0)
+                current_angle += 1.0
+
+                vis.poll_events()
+                vis.update_renderer()
+
+    utils.create_obj_file(triangles)
 
 
 def remove_inside_mesh(vertices, faces):
@@ -63,7 +145,7 @@ def remove_inside_mesh(vertices, faces):
             if remove_list.get((x, y, z), False):
                 continue
             triangle = [vertices[x], vertices[y], vertices[z]]
-            intersect = ray_intersects_triangle(center, vector, triangle)
+            intersect = math_utils.ray_intersects_triangle(center, vector, triangle)
             if intersect is not None:
                 d1 = distance_function(intersect, center)
                 d2 = distance_function(vertices[i], center)
